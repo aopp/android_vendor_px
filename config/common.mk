e5c68929b58c154c80f211388369f1a33d8ce1be
Timothy Mossey <tim@pwnieexpress.com>
2016-08-29 16:18:01 -0400

    Enable root usb debugging on experimental builds

--- config/common.mk
+++ config/common.mk
@@ -36,6 +36,15 @@ ifeq (,$(findstring $(TARGET_PRODUCT),aopp_deb aopp_hammerhead))
     PRODUCT_PACKAGES += BlueHydra
 endif
 
+# Enable root usb debugging on experimental builds
+ifeq ($(RELEASE_TYPE),EXPERIMENTAL)
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+        persist.sys.usb.config=mtp,adb \
+        ro.adb.secure=0 \
+        ro.secure=0 \
+        ro.debuggable=1
+endif
+
 # Required Pwnix env scripts
 PRODUCT_COPY_FILES += \
     vendor/pwnieexpress/prebuilt/common/bin/bootpwn:system/bin/bootpwn \
